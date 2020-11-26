0af4dc04b1c594bfaf8b5e7858c6e6c3ba1b89d1
Abir Hasan <hackitabirhasan@gmail.com>
2020-11-26 12:28:27 +0600

    Add files via upload

---
+++ tobrot/config.py
@@ -0,0 +1,25 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = "1456964191:AAFLkOh48xbnOtRUwlvCv-q2JObLA7vJsOg"
+  APP_ID = 1878625
+  API_HASH = "3ab3e48ba126de2eb96ffff39b18fa09"
+  AUTH_CHANNEL = [-1001256038785, -1001408445549, -1001398883981, -1001275361660, -1001273851623]
+  INDEX_LINK = "https://tgindex.hackitabirhasan.workers.dev/3:/Uploads"
+  YTDL_COMMAND = "ytdl@Leechz_Robot"
+  LEECH_COMMAND = "leech@Leechz_Robot"
+  CLEAR_THUMBNAIL = "clearthumbnail@Leechz_Robot"
+  SAVE_THUMBNAIL = "savethumbnail@Leechz_Robot"
+  OWNER_ID = 715779594
+  RCLONE_CONFIG = """type = drive
+scope = drive
+root_folder_id = 1rWomzK1yVjkqIbC9p9EBBKJgcf3imGr4
+token = {"access_token":"ya29A0AfH6SMCaLwuWkNALk5I4IWBZP6y5Y4lZRkgepXoaf-LvSu2jmNDyLzezwJk-HlgcP1OSiRKqAa9qKsCBMv9O2IMd1ebAwhYdVoNP7jIXMRBDKzD10qfrzq7oGda_Z-vP3Qvl6PQcOYC3x5ecEkU3vMIyqEC2xgANsEnowpSENeU","token_type":"Bearer","refresh_token":"1//0gHp6knwySTm9CgYIARAAGBASNwF-L9IrRPO94mU1d9kBkUu1BbYrkA_EMhGXcCiHXGULiAZDg5BmaSxhrtALRfBmVpiotPEtK6Q","expiry":"2020-11-11T13:38:54.4701885+06:00"}
+team_drive = 1rWomzK1yVjkqIbC9p9EBBKJgcf3imGr4"""
+  DESTINATION_FOLDER = ""
+  GLEECH_COMMAND = "gleech@Leechz_Robot"
+  TELEGRAM_LEECH_COMMAND_G = "tleech@Leechz_Robot"
+  PYTDL_COMMAND_G = "pytdl@Leechz_Robot"
+  MAX_TG_SPLIT_FILE_SIZE = 2097152000
+  CANCEL_COMMAND_G = "cancel@Leechz_Robot"
+  STATUS_COMMAND = "status@Leechz_Robot"
