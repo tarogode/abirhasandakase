960af51298ac98e88fdd9b5ece8e36a421ac215a
tarogode <65435259+tarogode@users.noreply.github.com>
2020-12-13 18:57:56 +0800

    Add files via upload

--- tobrot/config.py
+++ tobrot/config.py
@@ -1,25 +1,13 @@
 from tobrot.sample_config import Config
+#Fill your all data, read readme for reference
 
 class Config(Config):
-  TG_BOT_TOKEN = "1456964191:AAFLkOh48xbnOtRUwlvCv-q2JObLA7vJsOg"
-  APP_ID = 1878625
-  API_HASH = "3ab3e48ba126de2eb96ffff39b18fa09"
-  AUTH_CHANNEL = [-1001256038785, -1001408445549, -1001398883981, -1001275361660, -1001273851623]
-  INDEX_LINK = "https://tgindex.hackitabirhasan.workers.dev/3:/Uploads"
-  YTDL_COMMAND = "ytdl@Leechz_Robot"
-  LEECH_COMMAND = "leech@Leechz_Robot"
-  CLEAR_THUMBNAIL = "clearthumbnail@Leechz_Robot"
-  SAVE_THUMBNAIL = "savethumbnail@Leechz_Robot"
-  OWNER_ID = 715779594
-  RCLONE_CONFIG = """type = drive
-scope = drive
-root_folder_id = 1rWomzK1yVjkqIbC9p9EBBKJgcf3imGr4
-token = {"access_token":"ya29A0AfH6SMCaLwuWkNALk5I4IWBZP6y5Y4lZRkgepXoaf-LvSu2jmNDyLzezwJk-HlgcP1OSiRKqAa9qKsCBMv9O2IMd1ebAwhYdVoNP7jIXMRBDKzD10qfrzq7oGda_Z-vP3Qvl6PQcOYC3x5ecEkU3vMIyqEC2xgANsEnowpSENeU","token_type":"Bearer","refresh_token":"1//0gHp6knwySTm9CgYIARAAGBASNwF-L9IrRPO94mU1d9kBkUu1BbYrkA_EMhGXcCiHXGULiAZDg5BmaSxhrtALRfBmVpiotPEtK6Q","expiry":"2020-11-11T13:38:54.4701885+06:00"}
-team_drive = 1rWomzK1yVjkqIbC9p9EBBKJgcf3imGr4"""
-  DESTINATION_FOLDER = ""
-  GLEECH_COMMAND = "gleech@Leechz_Robot"
-  TELEGRAM_LEECH_COMMAND_G = "tleech@Leechz_Robot"
-  PYTDL_COMMAND_G = "pytdl@Leechz_Robot"
-  MAX_TG_SPLIT_FILE_SIZE = 2097152000
-  CANCEL_COMMAND_G = "cancel@Leechz_Robot"
-  STATUS_COMMAND = "status@Leechz_Robot"
+    TG_BOT_TOKEN= "1423837215:AAHD2S1EoTkPEtefGwNJwalD1rO-wtPv0JA"
+    APP_ID = 2533831
+    API_HASH = "cd35733b01c7da53a462e14f048291b4"
+    OWNER_ID = 1323460577
+    AUTH_CHANNEL = [-1001396933879]
+    DESTINATION_FOLDER = "TorrentLeech-Gdrive" #Name of your folder read readme(not id of the folder)
+    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.a0AfH6SMCA97PSbVnmQGcDS_JhHXcOw0CySDGxjzZyWjmrgWHDf9A1n-Rmdg4sVnUDxV9Y5_5tAdbhfpW6_xbF1O9ALAeBj1xmKFQtMubDyGfYB_i1LltJA_e_zJawP-peIK4yzoYrZTvdfP02U2PHfqHiV6WmX5RCUPmXOqHlMG4","token_type":"Bearer","refresh_token":"1//0guQVZQz65RjnCgYIARAAGBASNwF-L9IrWy7b2vaAF3Qyh-UjCszb0xVSG83KYCi4X9taiSfr_9EhDO13oqWNTuoVGris4xENImo","expiry":"2020-12-03T13:41:27.382397538Z"}"""
+    #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
+    #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
